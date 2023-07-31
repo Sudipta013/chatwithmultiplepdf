9d42af45a0af2eba9fb728e125aff2a62c3b98e8
Sudipta paul <Sudiptopaul403@gmail.com>
2023-07-31 21:52:30 +0530

    multipdfchat.py

--- multipdfchat.py
+++ multipdfchat.py
@@ -83,37 +83,59 @@ def main():
     bg = """
         <style> [data-testid="stAppViewContainer"]
         {
-            background: rgb(113,25,192);
-            background: linear-gradient(90deg, rgba(113,25,192,0.9903420840992647) 5%, 
-            rgba(41,59,181,1) 50%, rgba(143,0,255,1) 95%);
+            background: rgb(6,36,39);
         }
         </style>
         """
     sb = """
         <style>[data-testid="stSidebar"]
         {
-        background: rgb(131,58,180);
-        background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);
+        background: rgb(42, 52, 65);
         }
         </style>
     """
     st.markdown(sb,unsafe_allow_html=True)
     st.markdown(bg, unsafe_allow_html=True)
+    
+    # Add the yellow bottom bar
+    bottom_bar_html = """
+    <style>
+    .bottom-bar {
+        background-color: #FFA500;
+        padding: 5px;
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        text-align: center;
+        font-family: 'Russo One';
+        font-size: 20px;
+    }
+    </style>
+    <div class="bottom-bar">
+        <span style="color: white; font-weight: bold;">The Techie Indians</span>
+    </div>
+    """
+    st.markdown(bottom_bar_html, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("Chat with multiple PDFs :books:")
+    st.markdown("<h1 style='text-align: center; font-family:Abril Fatface ; -webkit-text-stroke: 1px black ;font-size: 50px; padding-bottom: 15px; color: rgb(255, 255, 255) ;'>Chat with multiple PDFs</h1>", unsafe_allow_html=True)
+    st.markdown("""<h5 style='text-align: center;font-size:18px;color: rgba(255,255,255,0.4); padding-top: 15px'>
+                Chat with multiple PDF AI tool is an interactive application that allows users to upload and communicate with multiple PDF documents using large language models. 
+                The tool facilitates natural language-based interactions and offers a user-friendly interface to extract and converse with the content of the uploaded PDFs in real-time.
+                </h5>""",unsafe_allow_html=True)
     user_question = st.text_input("Ask a question about your documents:")
     if user_question:
         handle_userinput(user_question)
 
     with st.sidebar:
-        st.title("Your documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        st.markdown("<h1 style='text-align: Left; font-family:Abril Fatface ;font-size: 32px; padding-bottom: 1px; color: rgb(255,255,255) ;'>Your Documents:</h1>", unsafe_allow_html=True)
+        st.markdown("<h1 style='text-align: Left ;font-size: 18px; padding-bottom: 0px; color: rgb(255, 165, 0) ;'>Upload your file and click process</h1>", unsafe_allow_html=True)
+        pdf_docs = st.file_uploader("", accept_multiple_files=True)
         if st.button("Process"):
             with st.spinner("Processing"):
                 # get pdf text
